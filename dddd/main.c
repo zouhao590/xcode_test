c0d8b1328a8907d0e4c66416dd75b8c1222d13d1
zouhao590 <zouhao666@gmail.com>
2014-04-30 12:00:25 +0800

    Initial Commit

---
+++ dddd/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  dddd
+//
+//  Created by Mac mini on 14-4-30.
+//  Copyright (c) 2014年 VIATOM. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
